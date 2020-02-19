da882595c923c10496e61c44272fc1fe24799ff4
sandeep79033 <60578012+sandeep79033@users.noreply.github.com>
2020-02-19 23:09:15 +0530

    Add files via upload

---
+++ Interest_Calculator/src/main/java/com/epam/Interest_Calculator/Calculate.java
@@ -0,0 +1,7 @@
+package com.epam.Interest_Calculator;
+
+public interface Calculate {
+	public abstract double calculateSimpleInterest(double principleAmount, double time, double rateOfInterest);
+	
+	public abstract double calculateCompoundInterest(double principleAmount, double time, double rateOfInterest);
+}
